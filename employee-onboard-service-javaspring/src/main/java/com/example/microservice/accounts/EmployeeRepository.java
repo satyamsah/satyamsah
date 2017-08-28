7c41dad39b5725d34ebb8f030e3176044fdb2491
satyamsah <kr.satyam.ind@gmail.com>
2017-08-28 19:07:59 -0400

    delete all the enries

--- employee-onboard-service-javaspring/src/main/java/com/example/microservice/accounts/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.microservice.accounts;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Integer>  {
-
-}
--- sqlscript/create-salaryslab.sql
+++
@@ -1,5 +0,0 @@
-create database salarydb;
-create user 'salaryuser'@'localhost' identified by 'ThePassword';
-grant all on salarydb.* to 'salaryuser'@'localhost';
-use salarydb;
-CREATE TABLE `salaryslab` (`salaryslabId` int(11) NOT NULL AUTO_INCREMENT ,`designation` varchar(255) NOT NULL,`dept` varchar(255) NOT NULL, `pay` varchar(255) NOT NULL, PRIMARY KEY (`salaryslabId`)) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
