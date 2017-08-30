a88b2316445db63e8c0634806042a53ce6139263
satyamsah <kr.satyam.ind@gmail.com>
2017-08-30 19:32:36 -0400

    clean delete

--- assignment1/sqlscript/create-employee.sql
+++
@@ -1,5 +0,0 @@
-create database employeedb;
-create user 'employeeuser'@'localhost' identified by 'ThePassword';
-grant all on db.* to 'employeeuser'@'localhost';
-CREATE TABLE `employee` (`id` int(11) NOT NULL AUTO_INCREMENT ,`name` varchar(255) NOT NULL,`dept` varchar(255) NOT NULL, `designation` varchar(255) NOT NULL,`gender` varchar(255) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
-use employee;
