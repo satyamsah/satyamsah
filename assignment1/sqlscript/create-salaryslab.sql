6a27a7e84eb09163e96c26d2f1a19139ac437a9d
Kumar Satyam <kr.satyam.ind@gmail.com>
2017-08-30 20:15:39 -0400

    Update create-salaryslab.sql

--- assignment1/sqlscript/create-salaryslab.sql
+++ assignment1/sqlscript/create-salaryslab.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS salarydb;
-create user 'salaryuser'@'localhost' identified by 'ThePassword';
+CREATE USER IF NOT EXISTS 'salaryuser'@'localhost' identified by 'ThePassword';
 grant all on salarydb.* to 'salaryuser'@'localhost';
 use salarydb;
 CREATE TABLE IF NOT EXISTS `salaryslab` (`salaryslabId` int(11) NOT NULL AUTO_INCREMENT ,`designation` varchar(255) NOT NULL,`dept` varchar(255) NOT NULL, `pay` varchar(255) NOT NULL, PRIMARY KEY (`salaryslabId`)) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
